7c6454f159362d71e54cc6859678d5df203b94e6
Michael Watson <developercoffeeshop@gmail.com>
2015-06-10 09:30:06 +0100

    Added the image as an attachment

--- upload.php
+++ upload.php
@@ -118,9 +118,11 @@ class qqFileUploader {
 	   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
 	}
     function handleUpload($uploadDirectory, $replaceOldFile = FALSE){
-        
+        //echo 'hit';
+        //exit();
 		if (!is_writable($uploadDirectory)){
-            return false;//array('error' => "Server error.");
+            //return false;
+            array('error' => "Server error.");
         }
         
         if (!$this->file){
@@ -171,17 +173,62 @@ class qqFileUploader {
 }
 function uploadScreen(){
 	$url = plugins_url();
-	wp_enqueue_script('fileuploader', $url . 'valumsplugin/js/fileuploader/fileuploader.js', array(), '1.0.0', true );
-	wp_enqueue_script('upload', $url.'valumsplugin/js/fileuploader/uploader.js', array(), '1.0.0', true );
-	wp_enqueue_style('upload', $url.'valumsplugin/css/fileuploader/fileuploader.css');
-
+	wp_enqueue_script('fileuploader', $url . '/valumsplugin/js/fileuploader/fileuploader.js', array(), '1.0.0', true );
+	wp_enqueue_script('upload', $url.'/valumsplugin/js/fileuploader/uploader.js', array(), '1.0.0', true );
+	wp_enqueue_style('upload', $url.'/valumsplugin/css/fileuploader/fileuploader.css');
+    $html = "<div class=\"progress active imagesProgress\">";
+    $html .="<div class=\"progress-bar ipb progress-bar-success\" role=\"progressbar\" aria-valuenow=\"0\" aria-valuemin=\"0\" aria-valuemax=\"100\">";
+    $html .="</div>";
+    $html .="</div>";
+    $html .="<div id=\"file-uploader\">";       
+    $html .="<noscript>";
+    $html .="<p>Please enable JavaScript to use file uploader.</p>";
+    $html .="<!-- or put a simple form for upload here -->";
+    $html .="</noscript>";
+    $html .="</div>";
+    return $html;
 }
 add_shortcode("swip_upload_button", "uploadScreen");  
-function handleUpload(){
+
+function uploadFiles(){
+
 	$allowedExtensions = array('jpg','jpeg','png','gif');
 	// max file size in bytes
 	$sizeLimit = 500 * 1024 * 1024;
 	$uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
-	$result = $uploader->handleUpload('assets/uploads/');
+    //print_r($uploader);
+    $dir = wp_upload_dir();
+	$result = $uploader->handleUpload($dir['path'].'/');
+    $result['url'] = $dir['url'].'/';
+    // $filename should be the path to a file in the upload directory.
+    $filename = $dir['path'].'/'.$result['filename'];
+    // Check the type of file. We'll use this as the 'post_mime_type'.
+    $filetype = wp_check_filetype( basename( $filename ), null );
+    // Get the path to the upload directory.
+    $wp_upload_dir = wp_upload_dir();
+    // Prepare an array of post data for the attachment.
+    $attachment = array(
+        'guid'           => $wp_upload_dir['url'] . '/' . basename( $filename ), 
+        'post_mime_type' => $filetype['type'],
+        'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
+        'post_content'   => '',
+        'post_status'    => 'inherit'
+    );
+    // Insert the attachment.
+    $attach_id = wp_insert_attachment( $attachment, $filename, $parent_post_id );
+    // Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
+    require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+    // Generate the metadata for the attachment, and update the database record.
+    $attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
+    wp_update_attachment_metadata( $attach_id, $attach_data );
+
+    $result['attach_id'] = $attach_id;
+    echo json_encode($result);
+
+    exit();
+
 }
+add_action( 'wp_ajax_upload_image', 'uploadFiles' );
+add_action( 'wp_ajax_nopriv_upload_image', 'uploadFiles' );
 ?>
